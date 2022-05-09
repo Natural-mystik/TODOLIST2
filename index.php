ce223bdd875fa932b9159a6041ad1ae17b60fd21
Natural-mystik <82944383+Natural-mystik@users.noreply.github.com>
2022-05-09 10:15:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -46,6 +46,6 @@ session_start();
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-      <!--https://www.youtube.com/watch?v=8gObpKGWLFU&list=PLoMsOxGlCKvCaK2OW3WjrYwLntDg0CxZK&index=3 minutes24-->
+
   </body>
 </html>
