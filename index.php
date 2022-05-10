a10d87190d578e0d7f17c90797a31f7f476952cc
Natural-mystik <82944383+Natural-mystik@users.noreply.github.com>
2022-05-10 15:43:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -15,10 +15,11 @@ session_start();
   </head>
   <body>
     <div class="container col-xl-10 col-xxl-8 px-4 py-5">
+      
       <div class="row align-items-center g-lg-5 py-5">
         <div class="col-lg-7 text-center text-lg-start">
           <h1 class="display-4 fw-bold lh-1 mb-3">Inscrivez-vous pour enregistrer vos todo</h1>
-          <p class="col-lg-10 fs-4">Creer gratuitement votre compte MYTOTOOL.</p>
+          <p class="animate-charcter">Creer gratuitement votre compte MYTOTOOL.</p>
         </div>
         <div class="col-md-10 mx-auto col-lg-5">
           <form action="login.php" method="POST" class="p-4 p-md-5 border rounded-3 bg-light">
@@ -37,15 +38,10 @@ session_start();
         </div>
       </div>
     </div>
-    
-    
-
-    
-    
+       
     
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
 
   </body>
 </html>
